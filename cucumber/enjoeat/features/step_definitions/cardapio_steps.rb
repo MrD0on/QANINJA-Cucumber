87bf9ec9d45cfcab5d4a30a10d33b33dd356c327
Ruan <ruan.sbs@hotmail.com>
2019-12-18 22:06:19 -0300

    refactor page object and create view cart

--- cucumber/enjoeat/features/step_definitions/cardapio_steps.rb
+++ cucumber/enjoeat/features/step_definitions/cardapio_steps.rb
@@ -1,12 +1,12 @@
 Dado("que acesso a lista de restaurantes") do
-  visit "/restaurants"
+  @rest_list_page.load
 end
 Quando("eu escolho o restaurante {string}") do |restaurante|
-  find(".restaurant-item", text: restaurante.upcase).click
+  @rest_list_page.go(restaurante)
 end
 
 Então("vejo os seguintes itens no cardápio:") do |table|
-  itens = all(".menu-item-info-box")
+  itens = @rest_page.menu
   product_data = table.hashes
   product_data.each_with_index do |value, index|
     expect(itens[index]).to have_text value["produto"].upcase
@@ -17,9 +17,8 @@ end
 
 Então("eu vejo as seguintes informações adicionais:") do |table|
   infos = table.rows_hash
-  detail = find('#detail')
-  expect(detail).to have_text infos['categoria']
-  expect(detail).to have_text infos['descricao']
-  expect(detail).to have_text infos['horarios']
-
+  detail = @rest_page.details
+  expect(detail).to have_text infos["categoria"]
+  expect(detail).to have_text infos["descricao"]
+  expect(detail).to have_text infos["horarios"]
 end
