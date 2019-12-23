8b841dc03b953393ec8457df81f7cee8bc4789c4
Ruan Santos <ruan.sbs@hotmail.com>
2019-12-23 10:45:36 -0300

    using screenxote

--- cucumber/enjoeat/features/support/env.rb
+++ cucumber/enjoeat/features/support/env.rb
@@ -3,7 +3,19 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  @browser = ENV["BROWSER"]
+  case @browser
+  when "chrome"
+    @driver = :selenium_chrome
+  when "firefox"
+    @driver = :selenium
+  when "chrome_headless"
+    @driver = :chrome_headless
+  when "firefox_hedless"
+    @driver = :firefox_headless
+  end
+
+  config.default_driver = @driver
   config.app_host = "http://localhost:3000"
   config.default_max_wait_time = 10
 end
--- cucumber/enjoeat/features/support/hooks.rb
+++ cucumber/enjoeat/features/support/hooks.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 Before do
   page.current_window.resize_to(1440, 900)
   @rest_list_page = RestaurantListPage.new
@@ -11,4 +13,12 @@ end
 
 Before("@green_food") do
   visit "/restaurants/green-food/menu"
+end
+
+After do |scenario|
+  if scenario.failed?
+    shot_file = page.save_screenshot("log/screenshot.png")
+    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+    embed(shot_b64, "image/png", "Screenshot") #Cucumber anexa o screenshot no report
+  end
 end
